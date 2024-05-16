a112c6f7cc877b438ae947bc738d66b1bde7b5d4
Nicholas Bergesen <nicholasbergesen@github.com>
2024-05-16 23:30:09 +0900

    Add Boulder data models

---
+++ boulderlog/Data/Models/RouteLog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace boulderlog.Data.Models
+{
+    [Table("RouteLog")]
+    public class RouteLog
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        public DateTime TimeStamp { get; set; }
+
+        [MaxLength(255), Required]
+        public string Type { get; set; }
+    }
+}
