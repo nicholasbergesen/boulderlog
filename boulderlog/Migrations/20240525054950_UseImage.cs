54ee3b69c4b65f87b5e629364e3ef18791badbf1
Nicholas Bergesen <nicholasbergesen@github.com>
2024-05-25 15:08:13 +0900

    Add database migrations

---
+++ boulderlog/Migrations/20240525054950_UseImage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boulderlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Climb");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Image",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageId",
+                table: "Climb",
+                type: "TEXT",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Image");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Climb");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Climb",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
