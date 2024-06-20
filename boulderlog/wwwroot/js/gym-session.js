c9b0c192b55944641813043f6c6c0a4e2eff9bdf
Nicholas Bergesen <nicholasb.za@github.com>
2024-06-20 15:10:35 +0900

    show session modal on page load

---
+++ boulderlog/wwwroot/js/gym-session.js
@@ -0,0 +1,29 @@
+﻿async function getGymData() {
+    const selectedGymId = $("#SessionGym option:selected").val();
+    const request = new Request(`/api/gym/${selectedGymId}`);
+    let response = await fetch(request);
+    let body = await response.json();
+
+    $("#SessionGrade option").remove();
+    $("#SessionWall option").remove();
+
+    $.each(body.grade, function (i, v) {
+        $("#SessionGrade").append($('<option></option>').val(v.value).html(v.text));
+    });
+
+    $.each(body.wall, function (i, v) {
+        $("#SessionWall").append($('<option></option>').val(v.text).html(v.text));
+    });
+
+    var urlParams = new URLSearchParams(window.location.search);
+    const gradeId = urlParams.get("gradeId");
+    const wall = urlParams.get("wall");
+
+    if (gradeId) {
+        $(`#SessionGrade option[value='${gradeId}']`).attr("selected", "selected");
+    }
+
+    if (wall) {
+        $(`#SessionWall option[value='${wall}']`).attr("selected", "selected");
+    }
+}
