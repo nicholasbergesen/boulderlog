08f48ba796dee8c6dcc73a17f27ee19d5ba24a2e
Nicholas Bergesen <nicholasb.za@github.com>
2024-06-20 20:30:15 +0900

    run migrations on application startup

--- boulderlog/Program.cs
+++ boulderlog/Program.cs
@@ -117,6 +117,13 @@ namespace Boulderlog
 
             var app = builder.Build();
 
+            //apply database migrations
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
