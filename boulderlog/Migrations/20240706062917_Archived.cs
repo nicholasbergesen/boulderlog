044c6bafe9c3b1fd7654b30ca4a9e5ff61c0f611
Nicholas Bergesen <nicholasb.za@gmail.com>
2024-07-06 15:34:48 +0900

    Nick/archive (#40)
    * fix gym grade select
    
    * Add archived column
    
    ---------
    
    Co-authored-by: Nicholas Bergesen <nicholasbergesen@github.com>

---
+++ boulderlog/Migrations/20240706062917_Archived.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boulderlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class Archived : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Climb",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Climb");
+        }
+    }
+}
